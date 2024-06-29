7af13d7108aab47e395dac95b9ff5bcd38eed936
Amit790 <rajputa009@gmail.com>
2024-06-29 23:07:16 +0530

    updated

--- index.js
+++ index.js
@@ -28,20 +28,20 @@
 //     ]
 // };
 // All data stored in tasklist as array and stored in Local Storage too
-var state={
+const state={
     taskList:[],
 };
 
-const taskContents=document.querySelector(".task__contents");
-const taskModal=document.querySelector(".task__modal__body");
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
 // console.log(taskContents);
 // console.log(taskModal);
 
 // Template for the card on screen. 
 // Arrow function used.
 // element identifier key=${id} is been missing on a line 43rd
-const htmlTaskCOntents=({id,title,type,description,url})=>`
-<div class='col-md-6 col-lg-4 mt-3' id=${id}>
+const htmlTaskContent = ({id, title, type, description, url})=> `
+<div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
     <div class='card shadow-sm task__card'>
         <div class="card-header d-flex justify-content-end task__card__header">
 
@@ -71,15 +71,15 @@ const htmlTaskCOntents=({id,title,type,description,url})=>`
         </div>
 
         <div class='card-footer'>
-          <button class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+          <button class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask" id=${id}>Open Task</button>
         </div>
     </div>
 </div>
 `;
 
 // Modal Body on click of Open Task
-const htmlModalCOntents=({id,title,type,description,url})=>{
-    const date = new date(parseInt(id));
+const htmlModalContent = ({id, title, description, url}) => {
+    const date = new Date(parseInt(id));
     return `
     <div id=${id}>
          ${
@@ -89,40 +89,39 @@ const htmlModalCOntents=({id,title,type,description,url})=>{
           <strong class='text-muted text-sm'>
             Created on: ${date.toDateString()}
           </strong>
-          <h2 class='m-3'>${title}</h2>
+          <h2 class='my-3'>${title}</h2>
           <p class='text-muted'>${description}</p>
-    </div> ` 
+    </div> `; 
 };
 
 // Convert JSON format into string  for local storage
-const updateLocalStorage = ()=>{
+const updateLocalStorage = () => {
     localStorage.setItem(
         "task",
         JSON.stringify({
             tasks:state.taskList,
         })
-    )
+    );
 };
 // Load Initial Data
-// Converting sring into JSON format for local storage for rendering  the card of the screen
-const loadInitalData = ()=>{
-    const localStorageCopy =JSON.parse(localStorage.tasks);
+// Converting string into JSON format for local storage for rendering  the card of the screen
+const loadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task);
     if(localStorageCopy) state.taskList = localStorageCopy.tasks;
-    state.taskList.map((cardDate)=>{
-        taskContents.innerAdjacentHTML("beforeend",htmlTaskCOntents(cardDate));
+    state.taskList.map(()=>{
+        taskContents.insertAdjacentHTML("beforeend",htmlTaskContent());
     });
 };
 
-const handleSubmit = (event)=>{
-    const id=`${Date.now()}`;
+const handleSubmit = ()=>{
+    const id = `${Date.now()}`;
     const input = {
         url: document.getElementById("imageUrl").value,
         title: document.getElementById("taskTitle").value,
-        tags: document.getElementById("tags").value,
-        taskDescription: document.getElementById("taskDescription").value,
-
+        type: document.getElementById("tags").value,
+        description: document.getElementById("taskDescription").value,
     };
-    taskContents.innerAdjacentHTML("beforeend",htmlTaskCOntents({...input,id}));
+    taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({...input, id}));
     state.taskList.push({...input,id});
     updateLocalStorage();
 };
