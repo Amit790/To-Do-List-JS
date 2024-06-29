666f72671b9e20c89d6fd521b2e5547c7cd94254
Amit790 <rajputa009@gmail.com>
2024-06-29 10:44:04 +0530

    updadted files

--- index.js
+++ index.js
@@ -27,6 +27,7 @@
 //         }
 //     ]
 // };
+// All data stored in tasklist as array and stored in Local Storage too
 var state={
     taskList:[],
 };
@@ -38,6 +39,7 @@ const taskModal=document.querySelector(".task__modal__body");
 
 // Template for the card on screen. 
 // Arrow function used.
+// element identifier key=${id} is been missing on a line 43rd
 const htmlTaskCOntents=({id,title,type,description,url})=>`
 <div class='col-md-6 col-lg-4 mt-3' id=${id}>
     <div class='card shadow-sm task__card'>
@@ -59,9 +61,45 @@ const htmlTaskCOntents=({id,title,type,description,url})=>`
             url && 
             `<img width='100%' src=${url} alt='Card Image' class='card-image-top md-3 rounded-lg'>`
           }
+          <h4 class='card-title task__card__title'>${title}</h4>
+          <p class='description trim-3-lines text-muted'>${description}</p>
+          
+          <div class='tags text-white d-flex flex-wrap'>
+            <span classs='badge bg-primary m-1'>${type}</span>
+          </div>
 
         </div>
 
+        <div class='card-footer'>
+          <button class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+        </div>
     </div>
 </div>
 `;
+
+// Modal Body on click of Open Task
+const htmlModalCOntents=({id,title,type,description,url})=>{
+    const date = new date(parseInt(id));
+    return `
+    <div id=${id}>
+         ${
+            url && 
+            `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3'>`
+          }
+          <strong class='text-muted text-sm'>
+            Created on: ${date.toDateString()}
+          </strong>
+          <h2 class='m-3'>${title}</h2>
+          <p class='text-muted'>${description}</p>
+    </div> ` 
+};
+
+
+const updateLocalStorage = ()=>{
+    localStorage.setItem(
+        "tasky",
+        JSON.stringify({
+            tasks:state.taskList,
+        })
+    )
+};
