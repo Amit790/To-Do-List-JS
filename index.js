1adbe7d30f0b486f0e4a7b3e1e4fc59940402ecd
Amit Rajput <142147866+Amit790@users.noreply.github.com>
2025-04-18 12:45:46 +0000

    files updated

--- index.js
+++ index.js
@@ -28,8 +28,8 @@
 //     ]
 // };
 // All data stored in tasklist as array and stored in Local Storage too
-const state={
-    taskList:[],
+const state = {
+    taskList: [],
 };
 
 const taskContents = document.querySelector(".task__contents");
@@ -40,7 +40,7 @@ const taskModal = document.querySelector(".task__modal__body");
 // Template for the card on screen. 
 // Arrow function used.
 // element identifier key=${id} is been missing on a line 43rd
-const htmlTaskContent = ({id, title, type, description, url})=> `
+const htmlTaskContent = ({ id, title, type, description, url }) => `
 <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
     <div class='card shadow-sm task__card'>
         <div class="card-header d-flex justify-content-end task__card__header">
@@ -57,10 +57,9 @@ const htmlTaskContent = ({id, title, type, description, url})=> `
         
         <div class='card-body'>
 
-         ${
-            url && 
-            `<img width='100%' src=${url} alt='Card Image' class='card-image-top md-3 rounded-lg'>`
-          }
+         ${url &&
+    `<img width='100%' src=${url} alt='Card Image' class='card-image-top md-3 rounded-lg'>`
+    }
           <h4 class='card-title task__card__title'>${title}</h4>
           <p class='description trim-3-lines text-muted'>${description}</p>
           
@@ -78,20 +77,19 @@ const htmlTaskContent = ({id, title, type, description, url})=> `
 `;
 
 // Modal Body on click of Open Task
-const htmlModalContent = ({id, title, description, url}) => {
+const htmlModalContent = ({ id, title, description, url }) => {
     const date = new Date(parseInt(id));
     return `
     <div id=${id}>
-         ${
-            url && 
-            `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3'>`
-          }
+         ${url &&
+        `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3'>`
+        }
           <strong class='text-muted text-sm'>
             Created on: ${date.toDateString()}
           </strong>
           <h2 class='my-3'>${title}</h2>
           <p class='text-muted'>${description}</p>
-    </div> `; 
+    </div> `;
 };
 
 // Convert JSON format into string  for local storage
@@ -99,7 +97,7 @@ const updateLocalStorage = () => {
     localStorage.setItem(
         "task",
         JSON.stringify({
-            tasks:state.taskList,
+            tasks: state.taskList,
         })
     );
 };
@@ -107,13 +105,13 @@ const updateLocalStorage = () => {
 // Converting string into JSON format for local storage for rendering  the card of the screen
 const loadInitialData = () => {
     const localStorageCopy = JSON.parse(localStorage.task);
-    if(localStorageCopy) state.taskList = localStorageCopy.tasks;
-    state.taskList.map(()=>{
-        taskContents.insertAdjacentHTML("beforeend",htmlTaskContent());
+    if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+    state.taskList.map(() => {
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent());
     });
 };
 
-const handleSubmit = ()=>{
+const handleSubmit = () => {
     const id = `${Date.now()}`;
     const input = {
         url: document.getElementById("imageUrl").value,
@@ -121,8 +119,8 @@ const handleSubmit = ()=>{
         type: document.getElementById("tags").value,
         description: document.getElementById("taskDescription").value,
     };
-    taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({...input, id}));
-    state.taskList.push({...input,id});
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContent({ ...input, id }));
+    state.taskList.push({ ...input, id });
     updateLocalStorage();
 };
 //  hjfdskjfbskvfnaskgj
